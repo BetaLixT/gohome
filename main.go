0d6ce051d9bbb4d23572ce00807814a2cca7bed8
BetaLixT <dcruzalphin@gmail.com>
2022-02-09 06:32:31 +0400

    some setup

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	err := initLogger()
+	if err != nil {
+		fmt.Println(err)
+		panic("Failed to initialize logger")
+	}
+
+	err = loadConfig("dev", "BLT_GOHK", "./configs", false)
+	if err != nil {
+		fmt.Println(err)
+		panic("Failed to load config")
+	}
 }
+
+func initLogger () error {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return err
+	}
+	defer logger.Sync()
+
+
+	return nil
+}
+
+func loadConfig(
+	env string,
+	prfix string,
+	loc string,
+	fileRequired bool,
+) error {
+	// TODO Mechanism for defaults
+
+	// - Loading config from file
+	viper.SetConfigName(fmt.Sprintf("config.%s", env))
+	viper.AddConfigPath(loc)
+	err := viper.ReadInConfig()
+	if err != nil {
+		if fileRequired {
+			return err
+		}
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		} 
+	}
+
+	// - Loading config from env
+	viper.SetEnvPrefix(prfix)
+	viper.AutomaticEnv()
+	return nil
+}
+
